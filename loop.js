d34fbec57d79540ba62992fa8c3bedf5052bf2eb
SAGAR ROY <111957606+SagaRoy12@users.noreply.github.com>
2024-05-20 10:02:16 -0700

    Update loop.js
    added that a comment in line number 1

--- loop.js
+++ loop.js
@@ -1,4 +1,4 @@
-const prompt = require ("prompt-sync")()
+const prompt = require ("prompt-sync")()  // helps to take user inpt
 // let value = 1
 // let number = prompt("enter the value of number")
 // number = Number.parseInt(number)
